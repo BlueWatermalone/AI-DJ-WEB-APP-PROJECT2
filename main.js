042cc472ee1fba400388201e5eee120a5f1e1fa6
BlueWatermalone <71526538+BlueWatermalone@users.noreply.github.com>
2021-08-25 19:54:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+song1 = "";
+song2 = "";
+song1_status = "";
+song2_status = "";
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function preload() {
+    song = loadSound("music.mp3");
+    song = loadSound("music2.mp3");
+}
+
+function play() {
+    song.play();
+}
+
